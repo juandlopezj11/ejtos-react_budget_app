67efbaccf63f0a384ff4ca8e56c158773162eb44
juandlopezj11 <167490128+juandlopezj11@users.noreply.github.com>
2024-06-08 20:20:58 -0500

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseList = () => {
     const { expenses } = useContext(AppContext);
+
     return (
         <table className='table'>
               <thead className="thead-light">
@@ -10,6 +12,7 @@ const ExpenseList = () => {
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>
               <th scope="col">Delete</th>
             </tr>
           </thead>
@@ -21,4 +24,5 @@ const ExpenseList = () => {
         </table>
     );
 };
+
 export default ExpenseList;
